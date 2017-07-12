97ad6dcc7fd06df682ce0ccadb05fb717729e820
newage99 <rooger99@gmail.com>
2017-07-12 12:37:03 +0200

    5

--- rendezvous.py
+++ rendezvous.py
@@ -1,13 +1,40 @@
 import socket
+import threading
 
 HOST = ''
-PORT = 50008
+PORT = 50007
+connections = []
+close_program = False
 
-while True:
+socket.setdefaulttimeout(10)
+
+def accepter():
+
+    global close_program
 
     accept_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     accept_socket.bind((HOST,PORT))
     accept_socket.listen(1)
+    
+    while close_program == False:
 
-    conn, addr = accept_socket.accept()
-    print('Connected by',addr)
+        try:
+            conn, addr = accept_socket.accept()
+        except:
+            a=1
+        else:
+            connections.append((conn,addr))
+            print('Connected by',addr)
+
+accepter_t = threading.Thread(name='accepter', target=accepter)
+accepter_t.start()
+
+while close_program == False:
+
+    command = input()
+
+    if command == 'exit':
+        
+        close_program = True
+        print('Closing accepter...')
+        accepter_t.join()
