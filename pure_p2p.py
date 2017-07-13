666b46620e2636050139932784318c253061b02e
newage99 <rooger99@gmail.com>
2017-07-13 12:26:28 +0200

    6

--- pure_p2p.py
+++ pure_p2p.py
@@ -10,8 +10,21 @@ PORT = 50007
 connections = []
 recv_threads = []
 close_program = False
+connected_to_server = False
 
-socket.setdefaulttimeout(10)
+socket.setdefaulttimeout(8)
+
+accept_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connect_to_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+accept_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+connect_to_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+c_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+connect_to_socket.bind(('',PORT))
+accept_socket.bind(('',PORT))
+c_socket.bind(('',PORT))
 
 def recver(ip,port,socket):
 
@@ -29,72 +42,132 @@ def recver(ip,port,socket):
 def accepter():
 
         global close_program
-        global recv_threads
+        global accept_socket
 
-        accept_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        accept_socket.bind((HOST,PORT))
-        accept_socket.listen(1)
+        accept_socket.listen(5)
+        
+        already_connected = False
         
         while close_program == False:
 
                 try:
                         conn, addr = accept_socket.accept()                        
                 except:
-                        #print("accept() error: ",sys.exc_info()[0])
-                        a=1
+                        pass
                 else:
-                        print('Connected by',addr)
-                        connections.append((addr[0],conn))
-                        recv_thread = threading.Thread(target=recver, args=(addr[0],addr[1],conn,))
-                        recv_thread.start()
-                        recv_threads.append(recv_thread)
+                        print('Connected by',addr+' (accepter thread)')
+
+                        exists = False
+
+                        for i in connections:
+                                if i[0] == addr[0]:
+                                        exists = True
+
+                        if exists == False:
+                                connections.append((addr[0],conn))
+                                recv_thread = threading.Thread(target=recver, args=(addr[0],addr[1],conn,))
+                                recv_thread.start()
+                                recv_threads.append(recv_thread)
         
-                        
-accepter_thread = threading.Thread(name='accepter', target=accepter)
-accepter_thread.start()
+
+#accepter_thread = threading.Thread(name='accepter', target=accepter)
+#accepter_thread.start()
+
+connected_to_server = True
 
 while close_program == False:
-        
+
         command = input().split()
+
+        if len(command) > 0:
         
-        if command[0] == 'connect':
-                        
-                connect_to_ip = command[1]
-                connect_to_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                if command[0] == 'connect_server':
 
-                try:
-                        connect_to_socket.connect((connect_to_ip,PORT))
-                except Exception as e:
-                        print('Error connecting to '+connect_to_ip+': '+str(e))
-                else:
-                        print('Connected to '+connect_to_ip)
-                        connections.append((connect_to_ip,connect_to_socket))
-                        recv_thread = threading.Thread(target=recver, args=(connect_to_ip,PORT,connect_to_socket,))
-                        recv_thread.start()
-                        recv_threads.append(recv_thread)
+                        port = 0
 
-        elif command[0] == 'say':
+                        if len(command) == 3:
+                                port = int(command[2])
+                        elif len(command) == 2:
+                                port = PORT
 
-                if len(command) > 2:
-                        
-                        say_to_ip = command[1]
-                        msg = command[2].encode()
-
-                        for i in range(len(connections)):
-                                if connections[i][0] == say_to_ip:
-                                        connections[i][1].send(msg)
-                                        
-                else:
-                        print('Not all the arguments provided.')
+                        if port != 0:
                                 
-        elif command[0] == 'exit':
+                                connect_to_ip = command[1]
 
-                close_program = True
-                
-                print('Closing threads...')
-                
-                accepter_thread.join()
-                for r in recv_threads:
-                        r.join()
+                                try:
+                                        connect_to_socket.connect((connect_to_ip,port))
+                                except Exception as e:
+                                        print('Error connecting to '+connect_to_ip+': '+str(e))
+                                else:
+                                        print('Connected to '+connect_to_ip)
+                                        connections.append((connect_to_ip,connect_to_socket))                                
+                                        connected_to_server = True                                
+                        else:
+                                print('Command arguments error.')
+
+                elif command[0] == 'connect':
+
+                        port = 0
+
+                        if len(command) == 3:
+                                port = int(command[2])
+
+                        if port != 0:
+                                        
+                                connect_to_ip = command[1]
+                                cont = 0
+
+                                while cont <= 10:
+
+                                        print('Trying to connect to '+connect_to_ip+' (Attempt '+str(cont)+' of 10)')
+                                        try:
+                                                c_socket.connect((connect_to_ip,port))
+                                        except Exception as e:
+                                                print('Error connecting to '+connect_to_ip+': '+str(e))
+                                        else:
+                                                print('Connected to '+connect_to_ip+' (connect command)')
+                                                exists = False
+                                                        
+                                                for i in connections:
+                                                        if i[0] == connect_to_ip:
+                                                                exists = True
+
+                                                if exists == False:
+                                                        connections.append((connect_to_ip,c_socket))
+                                                        recv_thread = threading.Thread(target=recver, args=(connect_to_ip,port,c_socket,))
+                                                        recv_thread.start()
+                                                        recv_threads.append(recv_thread)
+
+                                                cont = 12
+
+                                        cont = cont + 1
+                                        time.sleep(20)
+                                        
+                        else:
+                                print('Command arguments error.')
+                                
+                elif command[0] == 'say':
+
+                        if len(command) > 2:
+                                
+                                say_to_ip = command[1]
+                                msg = command[2].encode()
+
+                                for i in range(len(connections)):
+                                        if connections[i][0] == say_to_ip:
+                                                connections[i][1].send(msg)
+                                                
+                        else:
+                                print('Not all the arguments provided.')
+                                        
+                elif command[0] == 'exit':
+
+                        close_program = True
                         
-                print('All threads closed.')
+                        print('Closing threads...')
+                        
+                        #accepter_thread.join()
+                        for r in recv_threads:
+                                r.join()
+                                
+                        print('All threads closed.')
